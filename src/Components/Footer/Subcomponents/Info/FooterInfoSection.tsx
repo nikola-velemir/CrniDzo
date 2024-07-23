0bc38c3a073016ac1c435c0dd0901b335d35ba64
nikola-velemir <vnexa021@gmail.com>
2024-07-23 19:37:31 +0200

    [wip/fix] footer content

--- src/Components/Footer/Subcomponents/Info/FooterInfoSection.tsx
+++ src/Components/Footer/Subcomponents/Info/FooterInfoSection.tsx
@@ -1,18 +1,16 @@
 import { useContext } from "react";
 import { FooterInfoContext } from "../../../Context/Providers/FooterInfoContext";
-import FooterLogo from "./FooterLogo";
+
 import FooterWorkTime from "./FooterWorkTime";
 
 const FooterInfoSection = () => {
   const infoContext = useContext(FooterInfoContext);
   return (
     <div className="col-lg-3">
-      <div className="row">
-        <FooterLogo image_src={infoContext.footer_image}></FooterLogo>
-        <FooterWorkTime
-          work_days_and_hours={infoContext.work_days_and_hours}
-        ></FooterWorkTime>
-      </div>
+      <FooterWorkTime
+        work_days_and_hours={infoContext.work_days_and_hours}
+        logo_src={infoContext.footer_image}
+      ></FooterWorkTime>
     </div>
   );
 };
