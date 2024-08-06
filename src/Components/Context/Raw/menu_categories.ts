453c61c038d79d7dfe2606ff68ed205a92d53546
nikola-velemir <vnexa021@gmail.com>
2024-08-06 21:16:06 +0200

    [fix] homepage responsiveness

--- src/Components/Context/Raw/menu_categories.ts
+++ src/Components/Context/Raw/menu_categories.ts
@@ -7,25 +7,25 @@ import { menu_themes } from "./menu_themes";
 export const menu_categories = [
   {
     title: "Burgeri",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum aliquam nisl posuere condimentum. Vestibulum et ex varius, viverra ligula at, volutpat est. Aenean eros nisl, interdum quis leo eu, dignissim posuere sapien. Vivamus eget molestie turpis. Morbi feugiat pharetra efficitur. ",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum aliquam nisl posuere condimentum.",
     theme_color: menu_themes.burgers,
     img_src: md1,
   },
   {
     title: "Tortilje",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum aliquam nisl posuere condimentum. Vestibulum et ex varius, viverra ligula at, volutpat est. Aenean eros nisl, interdum quis leo eu, dignissim posuere sapien. Vivamus eget molestie turpis. Morbi feugiat pharetra efficitur. ",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum aliquam nisl posuere condimentum.",
     theme_color: menu_themes.tortilas,
     img_src: md2,
   },
   {
     title: "Palacinke",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum aliquam nisl posuere condimentum. Vestibulum et ex varius, viverra ligula at, volutpat est. Aenean eros nisl, interdum quis leo eu, dignissim posuere sapien. Vivamus eget molestie turpis. Morbi feugiat pharetra efficitur. ",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum aliquam nisl posuere condimentum.",
     theme_color: menu_themes.pancakes,
     img_src: md3,
   },
   {
     title: "Cetvrto",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum aliquam nisl posuere condimentum. Vestibulum et ex varius, viverra ligula at, volutpat est. Aenean eros nisl, interdum quis leo eu, dignissim posuere sapien. Vivamus eget molestie turpis. Morbi feugiat pharetra efficitur. ",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum aliquam nisl posuere condimentum.",
     theme_color: menu_themes.stakes,
     img_src: md4,
   },
